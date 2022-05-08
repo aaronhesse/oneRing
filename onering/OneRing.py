59840d8a6995386b0201d0a8c85ba76d79dab7b2
aaron-hesse <aaronhesse@protonmail.com>
2022-05-07 23:48:21 -0700

    A lot of updates here. Added support for filtering with keys,operators and values. Includes some function renames, but still contains some debug print statements, to be removed in the future.

--- onering/OneRing.py
+++ onering/OneRing.py
@@ -1,4 +1,6 @@
 import requests
+import urllib.parse
+import json
 
 class Client(object):
 
@@ -10,81 +12,103 @@ class Client(object):
     def __init__(self, apiKey):
         self.g_apiKey = apiKey
 
-    def makeAuthorizedRequest(self, path):
-        self.makeRequest(path, True)
+    def makeAuthenticatedRequest(self, path, options=None):
+        self.makeRequest(path, True, options)
 
-    def makeRequest(self, path, authorized=False):
+    def makeRequest(self, path, authenticate=False, options=None):
+
         print(path)
-        if not authorized:
+
+        # { param -> { operator : "!=<>", "value" : "100"}}
+
+        if options:
+            queryParams = ""
+            for param in options:
+                if param != 'sort':
+                    queryParams += param + options[param]['operator'] + options[param]['value'] + "&"
+                else:
+                    queryParams += 'sort=' + options['sort']['criteria'] + ':' + options['sort']['order']
+            queryParams = urllib.parse.quote(queryParams[:-1])
+            path += '?' + queryParams
+
+            print(path)
+
+        if not authenticate:
             r = requests.get(self.BASE_URL + path)
         else:
             r = requests.get(self.BASE_URL + path, headers={'Authorization':"Bearer " + self.g_apiKey})
 
+        # Handle http errors and return that response to the caller.
+
         print(r.status_code)
         print(r.headers['content-type'])
         print(r.encoding)
-        print(r.text)
-        print(r.json())
+        print(json.dumps(r.json(), indent=4))
+        print(path)
 
 
-    # Book and Chapter functions:
+    # Book functions:
 
-    def getBook(self, id=None):
-        if id:
-            self.makeRequest("book/" + id)
-        else:
-            self.makeRequest("book/")
+    def getAllBooks(self, options=None):
+        self.makeRequest("book", False, options)
 
-    def getChapter(self, id):
-        self.makeRequest("book/" + id + "/chapter")
+    def getBookById(self, book_id):
+        self.getAllBooks({"_id": { "operator": "=", "value": book_id}})
 
-    def getChapter(self, id=None):
-        if id:
-            self.makeAuthorizedRequest("chapter/" + id)
-        else:
-            self.makeAuthorizedRequest("chapter")
+    def getBookByName(self, book_name):
+        self.getAllBooks({"name": { "operator": "=", "value": book_name}})
+
+
+
+    # Chapter functions
+
+    def getAllChapters(self, options=None):
+        self.makeAuthenticatedRequest("chapter", options)
+
+    def getAllChaptersOfBook(self, book_id):
+        self.makeRequest("book/" + book_id + "/chapter")
+
+    def getChapterById(self, chapter_id):
+        self.getAllChapters({"_id": { "operator": "=", "value": chapter_id}})
+
+    def getChapterByName(self, chapter_name):
+        self.getAllChapters({"name": { "operator": "=", "value": chapter_name}})
+
 
 
     # Movie functions:
 
-    def getMoviesWithLimit(self, limit):
-        self.makeAuthorizedRequest("movie?limit=" + str(limit))
+    def getAllMovies(self, options=None):
+        self.makeAuthenticatedRequest("movie", options)
 
-    def getMovies(self):
-        self.makeAuthorizedRequest("movie")
+    def getMovieById(self, movie_id):
+        self.getAllMovies({"_id": { "operator": "=", "value": movie_id}})
 
-    def getMovie(self, id):
-        self.makeAuthorizedRequest("movie/" + id)
+    def getMovieByName(self, movie_name):
+        self.getAllMovies({"name": { "operator": "=", "value": movie_name}})
+
 
 
     # Character functions:
 
-    def getCharactersWithLimit(self, limit):
-        self.makeAuthorizedRequest("character?limit=" + str(limit))
+    def getAllCharacters(self, options=None):
+        self.makeAuthenticatedRequest("character", options)
 
-    def getCharacter(self, id=None):
-        if id:
-            self.makeAuthorizedRequest("character/" + id)
-        else:
-            self.makeAuthorizedRequest("character")
+    def getCharacterQuotes(self, character_id):
+        self.makeAuthenticatedRequest("character/" + character_id + "/quote")
 
-    def getCharacterByName(self, name):
-        self.makeAuthorizedRequest("character?name=" + name)
+    def getCharacterById(self, character_id):
+        self.getAllCharacters({"_id": character_id})
 
-    def getCharactersNotNamed(self, name):
-        self.makeAuthorizedRequest("character?name!=" + name)
+    def getCharactersByName(self, character_name):
+        self.getAllCharacters({"name": character_name})
 
-    def getCharacterQuote(self, id):
-        self.makeAuthorizedRequest("character/" + id + "/quote")
 
 
     # Quote functions:
 
-    def getAllQuotesWithLimit(self, limit):
-        self.makeAuthorizedRequest("quote?limit=" + str(limit))
+    def getAllQuotes(self, options=None):
+        self.makeAuthenticatedRequest("quote", options)
 
-    def getAllQuotes(self):
-        self.makeAuthorizedRequest("quote")
-
-    def getQuote(self, id):
-        self.makeAuthorizedRequest("quote/" + id)
+    def getQuoteById(self, quote_id):
+        self.getAllQuotes({"_id": {'operator': '=', 'value': quote_id}})
