364e9bb1177c5b7630e63f7b2ca6c992671ec6b6
aaron-hesse <aaronhesse@protonmail.com>
2022-05-08 08:37:10 -0700

    Cleaned up code, removed some print statements, made some methods private with __. Added support for queries to support all of the functionality of the original API. Tested all the methods and made sure they all work.

--- onering/OneRing.py
+++ onering/OneRing.py
@@ -1,10 +1,7 @@
 import requests
 import urllib.parse
-import json
 
 class Client(object):
-
-    # internal and/or helper functions or variables:
 
     BASE_URL = "https://the-one-api.dev/v2/"
     g_apiKey = ""
@@ -12,103 +9,96 @@ class Client(object):
     def __init__(self, apiKey):
         self.g_apiKey = apiKey
 
-    def makeAuthenticatedRequest(self, path, options=None):
-        self.makeRequest(path, True, options)
+    def __makeRequest(self, **kwargs):
 
-    def makeRequest(self, path, authenticate=False, options=None):
-
-        print(path)
+        if "path" in kwargs:
+            path = kwargs['path']
 
         # { param -> { operator : "!=<>", "value" : "100"}}
 
-        if options:
-            queryParams = ""
-            for param in options:
-                if param != 'sort':
-                    queryParams += param + options[param]['operator'] + options[param]['value'] + "&"
-                else:
-                    queryParams += 'sort=' + options['sort']['criteria'] + ':' + options['sort']['order']
-            queryParams = urllib.parse.quote(queryParams[:-1])
-            path += '?' + queryParams
+        if "options" in kwargs:
+            options = kwargs['options']
+            if options:
+                queryParams = ""
+                for param in options:
+                    if param != 'sort':
+                        queryParams += param + options[param]['operator'] + options[param]['value'] + "&"
+                    else:
+                        queryParams += 'sort=' + options['sort']['criteria'] + ':' + options['sort']['order']
+                queryParams = urllib.parse.quote(queryParams[:-1])
+                path += '?' + queryParams
 
-            print(path)
+        if "authenticate" in kwargs:
+            headers = {}
+            if kwargs['authenticate']:
+                headers = {'Authorization':"Bearer " + self.g_apiKey}
 
-        if not authenticate:
-            r = requests.get(self.BASE_URL + path)
-        else:
-            r = requests.get(self.BASE_URL + path, headers={'Authorization':"Bearer " + self.g_apiKey})
+            return requests.get(self.BASE_URL + path, headers=headers).json()
 
-        # Handle http errors and return that response to the caller.
-
-        print(r.status_code)
-        print(r.headers['content-type'])
-        print(r.encoding)
-        print(json.dumps(r.json(), indent=4))
-        print(path)
 
 
     # Book functions:
 
-    def getAllBooks(self, options=None):
-        self.makeRequest("book", False, options)
+    def getAllBooks(self, options_dict=None):
+        return self.__makeRequest(path="book", authenticate=True, options=options_dict)
 
     def getBookById(self, book_id):
-        self.getAllBooks({"_id": { "operator": "=", "value": book_id}})
+        return self.getAllBooks({"_id": { "operator": "=", "value": book_id}})
 
     def getBookByName(self, book_name):
-        self.getAllBooks({"name": { "operator": "=", "value": book_name}})
+        return self.getAllBooks({"name": { "operator": "=", "value": book_name}})
 
 
 
     # Chapter functions
 
-    def getAllChapters(self, options=None):
-        self.makeAuthenticatedRequest("chapter", options)
+    def getAllChapters(self, options_dict=None):
+        return self.__makeRequest(path="chapter", authenticate=True, options=options_dict)
 
     def getAllChaptersOfBook(self, book_id):
-        self.makeRequest("book/" + book_id + "/chapter")
+        return self.__makeRequest(path="book/" + book_id + "/chapter", authenticate=True)
 
     def getChapterById(self, chapter_id):
-        self.getAllChapters({"_id": { "operator": "=", "value": chapter_id}})
+        return self.getAllChapters({"_id": { "operator": "=", "value": chapter_id}})
 
     def getChapterByName(self, chapter_name):
-        self.getAllChapters({"name": { "operator": "=", "value": chapter_name}})
+        return self.getAllChapters({"chapterName": { "operator": "=", "value": chapter_name}})
 
 
 
     # Movie functions:
 
-    def getAllMovies(self, options=None):
-        self.makeAuthenticatedRequest("movie", options)
+    def getAllMovies(self, options_dict=None):
+        return self.__makeRequest(path="movie", authenticate=True, options=options_dict)
 
     def getMovieById(self, movie_id):
-        self.getAllMovies({"_id": { "operator": "=", "value": movie_id}})
+        return self.getAllMovies({"_id": { "operator": "=", "value": movie_id}})
 
     def getMovieByName(self, movie_name):
-        self.getAllMovies({"name": { "operator": "=", "value": movie_name}})
+        return self.getAllMovies({"name": { "operator": "=", "value": movie_name}})
 
 
 
     # Character functions:
 
-    def getAllCharacters(self, options=None):
-        self.makeAuthenticatedRequest("character", options)
+    def getAllCharacters(self, options_dict=None):
+        return self.__makeRequest(path="character", authenticate=True, options=options_dict)
 
     def getCharacterQuotes(self, character_id):
-        self.makeAuthenticatedRequest("character/" + character_id + "/quote")
+        return self.__makeRequest(path="character/" + character_id + "/quote", authenticate=True)
 
     def getCharacterById(self, character_id):
-        self.getAllCharacters({"_id": character_id})
+        return self.getAllCharacters({"_id": { "operator": "=", "value": character_id }})
 
     def getCharactersByName(self, character_name):
-        self.getAllCharacters({"name": character_name})
+        return self.getAllCharacters({"name": { "operator": "=", "value": character_name}})
 
 
 
     # Quote functions:
 
-    def getAllQuotes(self, options=None):
-        self.makeAuthenticatedRequest("quote", options)
+    def getAllQuotes(self, options_dict=None):
+        return self.__makeRequest(path="quote", authenticate=True, options=options_dict)
 
     def getQuoteById(self, quote_id):
-        self.getAllQuotes({"_id": {'operator': '=', 'value': quote_id}})
+        return self.getAllQuotes({"_id": {'operator': '=', 'value': quote_id}})
